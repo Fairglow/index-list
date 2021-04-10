1a48857e2bea96ae6c54d8b2b88b26c0feeceb14
Stefan Lindblad <stefan.lindblad@linux.com>
2021-04-10 11:34:28 +0200

    Add another line of output, the response
    Change variable name `ndx` to `index`.
    
    Showcase the 3 get method variants.

--- examples/indexlist.rs
+++ examples/indexlist.rs
@@ -4,9 +4,13 @@ fn main() {
     let mut hello = vec!["Hello,", "world!"];
     let name = "IndexList";
     let mut list = IndexList::from(&mut hello);
-    let ndx = list.insert_last(name);
-    list.insert_before(ndx, "I");
-    list.insert_before(ndx, "am");
+    let index = list.insert_last(name);
+    list.insert_before(index, "I");
+    list.insert_before(index, "am");
     let parts: Vec<&str> = list.iter().map(|e| e.as_ref()).collect();
-    println!("{}", parts.join(" "));
+    println!("{}.", parts.join(" "));
+    println!("The {} -- {} {}!",
+             list.get(list.move_index(index, -3)).unwrap_or(&"?"),
+             list.get_first().unwrap_or(&"?"),
+             list.get_last().unwrap_or(&"?"));
 }
