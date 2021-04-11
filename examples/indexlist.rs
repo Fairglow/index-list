2df9fb20f834f5d27697df9afca4feb808c606d1
Stefan Lindblad <stefan.lindblad@linux.com>
2021-04-11 21:13:00 +0200

    Mutate the list and simplify code

--- examples/indexlist.rs
+++ examples/indexlist.rs
@@ -1,16 +1,28 @@
 use index_list::IndexList;
 
 fn main() {
-    let mut hello = vec!["Hello,", "world!"];
     let name = "IndexList";
-    let mut list = IndexList::from(&mut hello);
-    let index = list.insert_last(name);
-    list.insert_before(index, "I");
-    list.insert_before(index, "am");
+    let mut list = IndexList::<String>::from(
+        &mut vec!["Hello,".into(), "world!".into()]);
+    let index = list.insert_last(name.to_string());
+    list.insert_before(index, String::from("I"));
+    list.insert_before(index, String::from("am"));
     let parts: Vec<&str> = list.iter().map(|e| e.as_ref()).collect();
     println!("{}.", parts.join(" "));
     println!("The {} -- {} {}!",
-             list.get(list.move_index(index, -3)).unwrap_or(&"?"),
-             list.get_first().unwrap_or(&"?"),
-             list.get_last().unwrap_or(&"?"));
+             list.get(list.move_index(index, -3)).unwrap(),
+             list.get_first().unwrap(),
+             list.get_last().unwrap());
+    let mut index = list.first_index();
+    while index.is_some() {
+        list.get_mut(index).map(|s| {
+            *s = if s.chars().last().unwrap().is_ascii_punctuation() {
+                s.get(0..s.len()-1).unwrap()
+            } else {
+                &s
+            }.to_string().to_lowercase();
+        });
+        index = list.next_index(index);
+    }
+    println!("{}", list);
 }
