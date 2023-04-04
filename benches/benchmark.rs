c9e477ef80f89e52a03362afa3e44699c943ad05
Stefan Lindblad <stefan.lindblad@linux.com>
2023-04-04 18:37:31 +0200

    Change license to MPLv2
    This fixes issue #2, which was requested by user @seekstar.
    
    The license used to be `LGPL-2.1-or-later`.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::collections::vec_deque::VecDeque;
 use std::collections::LinkedList;
